54089ed0ef1e3ac4e348c675fd03bf38ef59a8bd
Mikhail Kozhin <mishaloki@gmail.com>
2012-04-01 19:05:54 +0400

    Implemented LuckEventManager draft

---
+++ Ability/Luck/LuckEventManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Human80Level.Ability.Luck
+{
+    public static class LuckEventManager
+    {
+        public static List<LuckEventMessage> getEventList ()
+        {
+            List<LuckEventMessage> eventList = new List<LuckEventMessage>();
+            return eventList;
+        }
+
+        public static void AddEventMessage(LuckEventMessage eventMessage)
+        {
+                        
+        }
+
+        public static void RemoveEventMessage (LuckEventMessage eventMessage)
+        {
+            
+        }
+
+        public static void UpdateEventMessage (LuckEventMessage eventMessage)
+        {
+            
+        }
+    }
+}
