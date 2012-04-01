04130874f634141fbf5b4366a102c207aa8c5614
Mikhail Kozhin <mishaloki@gmail.com>
2012-04-01 17:36:47 +0400

    Implemented navigation to PageProfile. Some modifications in project

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Human80Level.Utils;
 using Microsoft.Phone.Controls;
 
 namespace Human80Level
@@ -19,6 +20,23 @@ namespace Human80Level
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void btnNewProfile_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProfileManager.getProfile() != null )
+            {
+                //TODO replace all hardcode text with constants
+                bool createNew =
+                    (MessageBox.Show("All previous result will be removed. Are you sure?", "Confirm removing profile",
+                                     MessageBoxButton.OKCancel) == MessageBoxResult.OK);
+                if (!createNew)
+                {
+                    return;
+                }
+
+                this.NavigationService.Navigate(new Uri("/PageProfile.xaml",UriKind.Relative));
+            }
         }
     }
 }
