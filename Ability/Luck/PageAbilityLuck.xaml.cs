e323b29570aa164f3d885262abe7f67d7e2e9d6f
Mikhail Kozhin <mishaloki@gmail.com>
2012-04-01 20:32:56 +0400

    Implemented draft logic to open random image

--- Ability/Luck/PageAbilityLuck.xaml.cs
+++ Ability/Luck/PageAbilityLuck.xaml.cs
@@ -8,16 +8,43 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Human80Level.Ability.Luck;
 using Microsoft.Phone.Controls;
 
 namespace Human80Level
 {
     public partial class PageAbilityLuck : PhoneApplicationPage
     {
+        private const string CloverImageUrl = "/Images/Ability/Luck/clover.png";
+
+        private const string TrashImageUrl = "/Images/Ability/Luck/trash.png";
+
+        private const string DefaultEventMessage = "";
+
+        private bool isLuck;
+        
         public PageAbilityLuck()
         {
             InitializeComponent();
+        }
+
+        private void imgLeft_Hold(object sender, GestureEventArgs e)
+        {
+            BitmapImage bitmapImage = new BitmapImage(new Uri(GetRandomImageUrl(),UriKind.Relative));
+            Image image = sender as Image;
+            image.Source = bitmapImage;
+            
+        }
+
+        private string GetRandomImageUrl ()
+        {
+            Random random = new Random();
+            int value = random.Next(0, 2);           
+            isLuck = (value == 0) ? true : false;
+            string url = (isLuck) ? CloverImageUrl : TrashImageUrl;
+            return url;
         }
     }
 }
