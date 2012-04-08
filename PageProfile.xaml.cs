054b323afa2b1be7feed7a6c1609e7db6cd1a5d3
Mikhail Kozhin <mishaloki@gmail.com>
2012-04-08 14:32:55 +0400

    code refactror - added comments, remove logic from event handlers, added log

--- PageProfile.xaml.cs
+++ PageProfile.xaml.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.IsolatedStorage;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using Human80Level.Utils;
@@ -21,36 +11,122 @@ namespace Human80Level
 {
     public partial class PageProfile : PhoneApplicationPage
     {
+        /// <summary>
+        /// Path to avatar image in Isolated storage
+        /// </summary>
         private string avatarUrl = string.Empty;
-
        
+        /// <summary>
+        /// Task that opens phone gallery 
+        /// </summary>
+        private PhotoChooserTask photoChooserTask;
 
-        PhotoChooserTask photoChooserTask;
+        /// <summary>
+        /// Message box title for incorrect height
+        /// </summary>
+        private readonly string MBheightIncorrectTitle = "";
 
-        CameraCaptureTask cameraTask;
+        /// <summary>
+        /// Message box message for incorrect nickname
+        /// </summary>
+        private readonly string MBnickIncorrectMessage = "";
+
+        /// <summary>
+        /// Message box title for incorrect nickname
+        /// </summary>
+        private readonly string MBnickIncorrectTitle = "";
+
+        /// <summary>
+        /// Message box message for incorrect height
+        /// </summary>
+        private readonly string MBheightIncorrectMessage = "";
+
+        /// <summary>
+        /// Message box save image error title
+        /// </summary>
+        private readonly string MBsaveErrorTitle = "";
+
+        /// <summary>
+        /// Message box save image error message
+        /// </summary>
+        private readonly string MBsaveErrorMessage = "";
+
+        /// <summary>
+        /// Message box get image error title
+        /// </summary>
+        private readonly string MBgetErrorTitle = "";
+
+        /// <summary>
+        /// Message box get image error messasge
+        /// </summary>
+        private readonly string MBgetErrorMessage = "";
+
+        /// <summary>
+        /// Task that opens phone camera app
+        /// </summary>
+        private CameraCaptureTask cameraTask;
         
+        /// <summary>
+        /// Page constructor
+        /// </summary>
         public PageProfile()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Save button handler
+        /// </summary>
+        /// <param name="sender">Save button</param>
+        /// <param name="e">button event args</param>
         private void btnSaveProfile_Click(object sender, RoutedEventArgs e)
+        {            
+            this.SaveProfile();
+        }
+
+        /// <summary>
+        /// Gets profile data from Input fields and saves it to Isolated storage
+        /// </summary>
+        private void SaveProfile()
         {
             try
             {
+                if (string.IsNullOrEmpty(textNickName.Text))
+                {
+                    MessageBox.Show(MBnickIncorrectTitle, MBnickIncorrectMessage, MessageBoxButton.OK);
+                    return;
+                }
+                int height;
+                if (!int.TryParse(boxHeight.Text,out height))
+                {
+                    MessageBox.Show(MBheightIncorrectTitle, MBheightIncorrectMessage, MessageBoxButton.OK);
+                    return;
+                }
+                //todo Add datepicker icons
                 DateTime birth = dateBirth.Value.Value;
-                int height = int.Parse(boxHeight.Text);
                 Profile.Profile profile = new Profile.Profile(textNickName.Text, avatarUrl, 0, true, 0, birth, height);
                 ProfileManager.UpdateProfile(profile);
             }
             catch (Exception err)
             {
-                Logger.Error("ShowProfile", err.Message);
+                Logger.Error("SaveProfile", err.Message);
             }
- 
         }
 
+        /// <summary>
+        /// Empty button handler
+        /// </summary>
+        /// <param name="sender">empty button</param>
+        /// <param name="e">button event args</param>
         private void btnImageEmpty_Click(object sender, RoutedEventArgs e)
+        {
+            SetAvatarToEmptyImage();
+        }
+
+        /// <summary>
+        /// Resets avatar Uri and sets preview image source to null
+        /// </summary>
+        private void SetAvatarToEmptyImage()
         {
             try
             {
@@ -59,12 +135,15 @@ namespace Human80Level
             }
             catch (Exception err)
             {
-                Logger.Error("ShowProfile", err.Message);
+                Logger.Error("SetAvatarToEmptyImage", err.Message);
             }
-
         }
 
-        public void GetPicture(bool fromCamera)
+        /// <summary>
+        /// Gets image from camera or gallery
+        /// </summary>
+        /// <param name="fromCamera">does image have to be taken from camera or from gallery</param>
+        private void GetPicture(bool fromCamera)
         {
             try
             {
@@ -83,22 +162,20 @@ namespace Human80Level
             }
             catch (Exception err)
             {
-                Logger.Error("ShowProfile", err.Message);
+                MessageBox.Show(MBgetErrorTitle, MBgetErrorMessage, MessageBoxButton.OK);
+                Logger.Error("GetPicture", err.Message);
             }
-
-
         }
 
-        public void onTaskCompleted(object sender, PhotoResult e)
+        /// <summary>
+        /// Saves image to Isolated storage and sets preview image source to new image
+        /// </summary>
+        /// <param name="sender">camera or photo chosen task</param>
+        /// <param name="e"></param>
+        private void onTaskCompleted(object sender, PhotoResult e)
         {
             try
             {
-                if (e.Error != null)
-                {
-
-                    return;
-                }
-
                 switch (e.TaskResult)
                 {
                     case TaskResult.OK:
@@ -112,7 +189,7 @@ namespace Human80Level
                         }
                         catch (Exception ex)
                         {
-
+                            MessageBox.Show(MBsaveErrorTitle,MBsaveErrorMessage,MessageBoxButton.OK);
                         }
                         break;
 
@@ -125,27 +202,46 @@ namespace Human80Level
             }
             catch (Exception err)
             {
-                Logger.Error("ShowProfile", err.Message);
+                MessageBox.Show(MBgetErrorTitle, MBgetErrorMessage, MessageBoxButton.OK);
+                Logger.Error("onTaskCompleted", err.Message);
             }
             
         }
 
+        /// <summary>
+        /// Album button handler
+        /// </summary>
+        /// <param name="sender">album button</param>
+        /// <param name="e">button event args</param>
         private void btnImageAlbum_Click(object sender, RoutedEventArgs e)
         {
             this.GetPicture(false);
         }
 
+        /// <summary>
+        /// Camera button 
+        /// </summary>
+        /// <param name="sender">camera button</param>
+        /// <param name="e">button event args</param>
         private void btnImageCamera_Click(object sender, RoutedEventArgs e)
         {
             this.GetPicture(true);
         }
 
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            try
-            {
-                base.OnNavigatedTo(e);
+            base.OnNavigatedTo(e);    
+            CheckProfileExistence();
+        }
 
+        /// <summary>
+        /// Checks if new profile is needed and display profile data if it is already exist
+        /// </summary>
+        private void CheckProfileExistence()
+        {
+            try
+            {               
                 if (this.NavigationContext.QueryString.ContainsKey("new"))
                 {
 
@@ -157,12 +253,13 @@ namespace Human80Level
             }
             catch (Exception err)
             {
-                Logger.Error("ShowProfile", err.Message);
-            }
-
-            
+                Logger.Error("CheckProfileExistence", err.Message);
+            }        
         }
 
+        /// <summary>
+        /// Gets profile from app settings and displays profile data
+        /// </summary>
         private void ShowProfile()
         {
             try
@@ -181,7 +278,6 @@ namespace Human80Level
             {
                 Logger.Error("ShowProfile", e.Message);
             }
-
         }
     }
 }
