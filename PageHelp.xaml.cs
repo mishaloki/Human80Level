1f09b5bb8353bdaaee6880d9067ce6331b2173fb
Mikhail Kozhin <mishaloki@gmail.com>
2012-04-10 22:40:31 +0400

    Added log, comments, some refactor

--- PageHelp.xaml.cs
+++ PageHelp.xaml.cs
@@ -1,23 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
+﻿using Human80Level.Utils;
 using Microsoft.Phone.Controls;
 
 namespace Human80Level
 {
     public partial class PageHelp : PhoneApplicationPage
     {
+        /// <summary>
+        /// Help page constructor
+        /// </summary>
         public PageHelp()
         {
             InitializeComponent();
+            Logger.Info("PageHelp constructor", "page was initialized");
         }
     }
 }
